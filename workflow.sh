dee27eadace1fb3743993be6fdbd9d4b18c5003f
GabeAl <GabeAl@users.noreply.github.com>
2016-03-08 15:27:13 -0600

    Workflow script! V4 MySeq300_nextera

---
+++ workflow.sh
@@ -0,0 +1,11 @@
+module load soft/qiime/1.9.0
+export PATH=/project/flatiron/gabe/pmp:$PATH
+export PATH=/project/flatiron/gabe/NINJA_OPS_132/:$PATH
+export PATH=/project/flatiron/gabe/NINJA_OPS_132/bin:$PATH
+export PATH=/project/flatiron/gabe:$PATH
+
+axe_adaptors_nextera.sh
+create_flasher_strict_tc.sh
+create_trimmer_lengthOnly.sh
+make_fastas.sh
+make_map.sh
