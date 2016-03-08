dea2eafeb72a958e1e5b9341aa0c6071f801c5bb
GabeAl <GabeAl@users.noreply.github.com>
2016-03-08 15:16:21 -0600

    Added files via upload

---
+++ others/setup_core_div.sh
@@ -0,0 +1,5 @@
+mkdir $HOME/.config
+mkdir $HOME/.config/matplotlib
+mkdir $HOME/.matplotlib
+echo "backend      : Agg" > $HOME/.config/matplotlib/matplotlibrc
+echo "backend      : Agg" > $HOME/.matplotlib/matplotlibrc
